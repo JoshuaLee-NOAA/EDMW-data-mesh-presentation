bd781d867a11d4fed443608fa694c3d574046d0e
JLee2021 <22698564+joshua-daniel-lee@users.noreply.github.com>
2026-09-13 13:13:47 -0400

    Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/presentation-template" : "",
+  basePath: isProd ? "/EDMW-data-mesh-presentation" : "",
   images: {
     unoptimized: true,
   },
